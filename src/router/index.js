70394d424484633568751bbac4f9caf3fff27d9d
hyoseung <shs95@osstem.com>
2020-11-19 09:37:03 +0900

    commit test

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/draggableTodo',
